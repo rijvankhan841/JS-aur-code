77e3c7ad05a405189612ad0ec70e635183dbd0c9
Rizzu <rijvank118@gmail.com>
2025-09-01 08:03:40 +0530

    add: implement sayMyName and addTwoNum functions with example usage

---
+++ 03_Basics/functions.js
@@ -0,0 +1,22 @@
+function sayMyName(){
+    console.log("R")
+    console.log("I")
+    console.log("J")
+    console.log("V")
+    console.log("A")
+    console.log("N")
+}
+
+// sayMyName()
+
+function addTwoNum(n1, n2) {
+    // console.log(n1 + n2)
+
+    // let result = n1 + n2
+    // return result
+    return n1 + n2
+}
+
+const result = addTwoNum(3, 5)
+
+console.log("Result: ", result)
